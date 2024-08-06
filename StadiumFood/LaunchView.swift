e5144668f05917b37fa26343247c165748222a3f
2HYUNHO <soda9503@gmail.com>
2024-08-06 23:40:19 +0900

    [Fix] 런치스크린 애니메이션추가

--- StadiumFood/LaunchView.swift
+++ StadiumFood/LaunchView.swift
@@ -8,12 +8,29 @@
 import SwiftUI
 
 struct LaunchView: View {
+    @State var isLaunching: Bool = true
+    
+    var body: some View {
+        if isLaunching {
+            SplashView()
+                .onAppear {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        isLaunching = false
+                    }
+                }
+        } else {
+            TabBarView()
+        }
+    }
+}
+
+struct SplashView: View {
+    
     var body: some View {
         ZStack {
             Color("BG")
                 .ignoresSafeArea()
-            
-            Image("textLogo")
+            Image("AppFont")
         }
     }
 }
